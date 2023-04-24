71e1deb75f0684a17f8df9966d8a5dc4d13b8255
Zivilevs <zivileval@yahoo.com>
2023-04-23 21:00:25 -0300

    Error connecting to ms sql server from mssql-tools docker docker compose with 2 services. DB restoring works.

--- init_base.sh
+++ init_base.sh
@@ -1,1 +1,1 @@
-/opt/mssql-tools/bin/sqlcmd -S mssqlserver -U sa -P MsSqlServer2022! -d master -i /tmp/recreate_db.sql
+/opt/mssql-tools/bin/sqlcmd -S mssqlhost -U sa -P "MsSqlServer2022!" -d master -i /tmp/recreate_db.sql
