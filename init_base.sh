1657926aa23624f1070398bfbbda5fdd621fac70
Zivilevs <zivileval@yahoo.com>
2023-04-23 23:36:02 -0300

    Add docker ip and sleep, still not working

--- init_base.sh
+++ init_base.sh
@@ -1,1 +1,5 @@
-/opt/mssql-tools/bin/sqlcmd -S mssqlhost -U sa -P "MsSqlServer2022!" -d master -i /tmp/recreate_db.sql
+#!/bin/bash
+
+sleep 30
+
+/opt/mssql-tools/bin/sqlcmd -S 172.17.0.1 -U sa -P "MsSqlServer2022!" -d master -i /tmp/recreate_db.sql
