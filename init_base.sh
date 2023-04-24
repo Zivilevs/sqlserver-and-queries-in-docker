6db2585feaea749fa7cecf20fe9b60104188faac
Zivilevs <zivileval@yahoo.com>
2023-04-24 00:22:27 -0300

    Add mssqlserver container name and sleep workaround to give time for server bootup.

--- init_base.sh
+++ init_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sleep 30
+sleep 70
 
-/opt/mssql-tools/bin/sqlcmd -S 172.17.0.1 -U sa -P "MsSqlServer2022!" -d master -i /tmp/recreate_db.sql
+/opt/mssql-tools/bin/sqlcmd -S mssqlserver -U sa -P "MsSqlServer2022!" -d master -i /tmp/recreate_db.sql
