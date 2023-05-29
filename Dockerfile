ec9b09526df4a6719d09fe017a285a3239317d8f
Zivile <zivileval@yahoo.com>
2023-05-29 09:37:48 -0300

    Separate db restore and use pyodbc

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 FROM python:3.10-slim-bullseye
+#FROM ubuntu:20.04
 
 COPY ./code /var/code
 
 WORKDIR /var/code
 
+RUN apt update && apt install -y curl unixodbc gnupg2
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+RUN curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list && apt-get update
+
+RUN ACCEPT_EULA=Y apt-get install -y msodbcsql18
 RUN pip install --no-cache-dir --upgrade pip \
   && pip install --no-cache-dir -r requirements.txt
---
+++ code/restore_db.py
@@ -0,0 +1,42 @@
+import pyodbc
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+server = os.environ.get('ms_server')
+username = os.environ.get('ms_user')
+password = os.environ.get('ms_sa_password')
+database = os.environ.get('ms_default_database')
+
+print('passou')
+print(database)
+print(username)
+print(password)
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};'
+                      'SERVER=localhost,1434;'
+                      f'DATABASE={database};'
+                      'Encrypt=no;'
+                      f'UID={username};'
+                      f'PWD={password};')
+
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+restore = """USE [master];IF DB_ID('AdventureWorks2019') IS NULL
+RESTORE DATABASE [AdventureWorks2019]
+FROM DISK = '/var/opt/mssql/backup/AdventureWorks2019.bak'
+WITH
+    MOVE 'AdventureWorks2019' TO '/var/opt/mssql/data/AdventureWorks2019.mdf',
+    MOVE 'AdventureWorks2019_log' TO '/var/opt/mssql/data/AdventureWorks2019_log.ldf',
+    FILE = 1,
+    NOUNLOAD,
+    STATS = 5"""
+cursor.execute(restore)
+# print(cursor)
+cursor.close()
+print('ok')
