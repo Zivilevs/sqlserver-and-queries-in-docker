40da6aa89d0b61c29bb660c71b9cc12e564db7b2
Zivile <zivileval@yahoo.com>
2023-06-05 22:37:41 -0300

    Only read access

--- code/create_reporting_user.py
+++ code/create_reporting_user.py
@@ -29,7 +29,6 @@ CREATE LOGIN reporting WITH PASSWORD='Reporting123';
 USE AdventureWorks2019;
 CREATE USER reporting FOR LOGIN reporting;
 ALTER ROLE db_datareader ADD MEMBER reporting;
-ALTER ROLE db_datawriter ADD MEMBER reporting;
 END"""
 res = cursor.execute(restore)
 cursor.close()
